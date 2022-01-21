d2641ab87609f64b0c0d320ed792daeb6d4b7c0a
mat179 <mat179@users.noreply.github.com>
2022-01-22 00:40:17 +0300

    Task #3-2 Реализован http - сервер, отдающий файл по запросу пользователя

--- 03-streams/03-file-server-get/server.js
+++ 03-streams/03-file-server-get/server.js
@@ -1,6 +1,6 @@
-const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
@@ -11,10 +11,26 @@ server.on('request', (req, res) => {
   const filepath = path.join(__dirname, 'files', pathname);
 
   switch (req.method) {
-    case 'GET':
+    case 'GET': {
+      if (pathname.includes('/')) {
+        res.statusCode = 400
+        res.end('Nested paths not supported');
+      }
 
+      const readStream = fs.createReadStream(filepath)
+          .on('error', err => {
+            if (err.code === 'ENOENT'){
+              res.statusCode = 404
+              res.end('File not found');
+            } else{
+              res.statusCode = 500
+              res.end('Internal server error');
+            }
+          })
+
+      readStream.pipe(res);
       break;
-
+    }
     default:
       res.statusCode = 501;
       res.end('Not implemented');
