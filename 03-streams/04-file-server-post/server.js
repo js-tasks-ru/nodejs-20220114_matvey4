b4c1fc50ef2e16c8a7e9114d8ca59a906143dbcd
mat179 <mat179@users.noreply.github.com>
2022-01-25 02:06:58 +0300

    Task #3-4 Рефакторинг кода, тест на превышение допустимого размера файла так и не проходит

--- 03-streams/04-file-server-post/server.js
+++ 03-streams/04-file-server-post/server.js
@@ -1,4 +1,3 @@
-const url = require('url');
 const http = require('http');
 const path = require('path');
 const fs = require("fs");
@@ -11,60 +10,54 @@ server.on('request', (req, res) => {
 
     const filepath = path.join(__dirname, 'files', pathname);
 
+    const responseHandler = (code, message) => {
+        res.statusCode = code;
+        res.end(message);
+    }
+
     switch (req.method) {
         case 'POST':
 
             if (pathname.includes('/')) {
-                res.statusCode = 400
-                res.end('Nested paths not supported');
+                responseHandler(400,'Nested paths not supported');
                 break;
             }
 
-            fs.stat(filepath,(err => {
-                if (err === null) {
-                    res.statusCode = 409
-                    res.end('File already exists');
-                } else if (err.code === 'ENOENT') {
-                    const writeStream =  fs.createWriteStream(filepath);
-                    const limitedSizeStream = new LimitedSizeStream({limit: 1024*1024});
+            const writeStream =  fs.createWriteStream(filepath, {flags: 'wx'});
+            const limitedSizeStream = new LimitedSizeStream({limit:  1024*1024});
 
-                    req
-                        .on('aborted', () => {
-                            fs.unlink(filepath, () => {})
-                            writeStream.destroy();
-                        })
-                        .pipe(limitedSizeStream)
-                        .on('error', error => {
-                            res.statusCode = 413;
-                            res.end('File too large');
-                            writeStream.destroy();
-                            fs.unlink(filepath, () => {});
-
-                            //limitedSizeStream.destroy();
-
-                        })
-                        .pipe(writeStream)
-                        /*.on('error', error => {
-                            res.statusCode = 500
-                            res.end('Internal server error');
-                        })*/
-                        .on('close',() => {
-                            res.statusCode = 201
-                            res.end('Created');
-                            //req.destroy();
-                        });
-                }
-                else {
-                    res.statusCode = 500;
-                    res.end('Internal server error');
-                }
-            }))
+            req
+                .on('aborted', () => {
+                    writeStream.destroy();
+                    limitedSizeStream.destroy();
+                    fs.unlink(filepath, () => {})
+                })
+                .pipe(limitedSizeStream)
+                .on('error', error => {
+                    if (error.code === 'LIMIT_EXCEEDED') {
+                        responseHandler(413,'File too large');
+                    } else {
+                        responseHandler(500,'Internal server error');
+                    }
+                    writeStream.destroy();
+                    fs.unlink(filepath, () => {});
+                })
+                .pipe(writeStream)
+                .on('error', error => {
+                    if (error.code === 'EEXIST'){
+                        responseHandler(409,'File already exists');
+                    } else {
+                        responseHandler(500,'Internal server error');
+                    }
+                })
+                .on('finish',() => {
+                    responseHandler(201,'Created');
+                });
 
             break;
 
         default:
-            res.statusCode = 501;
-            res.end('Not implemented');
+            responseHandler(501,'Not implemented');
     }
 });
 
