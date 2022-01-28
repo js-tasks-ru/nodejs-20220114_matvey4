2fceb357d68b6db425d9c29621ea24ccbb8f7639
mat179 <mat179@users.noreply.github.com>
2022-01-28 14:54:08 +0300

    Task #4-1

--- 04-testing-configuration-logging/01-unit-tests/test/Validator.test.js
+++ 04-testing-configuration-logging/01-unit-tests/test/Validator.test.js
@@ -34,12 +34,6 @@ describe('testing-configuration-logging/unit-tests', () => {
       expect(errors[0]).to.have.property('error').and.to.be.equal('too long, expect 20, got 24');
     });
 
-    it('валидатор проверяет строковые поля (ok)', () => {
-
-      const errors = validator.validate({ name: 'LalalaLalalaLalala', age: 19  });
-
-      expect(errors).to.have.length(0);
-    });
 
     it('валидатор проверяет числовые поля (<min)', () => {
 
@@ -59,7 +53,7 @@ describe('testing-configuration-logging/unit-tests', () => {
       expect(errors[0]).to.have.property('error').and.to.be.equal('too big, expect 27, got 28');
     });
 
-    it('валидатор проверяет числовые поля (ok)', () => {
+    it('поля проходят проверку', () => {
 
       const errors = validator.validate({name: 'LalalaLalalaLalala',age: 19});
 
