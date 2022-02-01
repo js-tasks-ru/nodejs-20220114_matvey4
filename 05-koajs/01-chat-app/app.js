6ab17a93cba04411ba2e53a3e7466ab8031de0c1
mat179 <mat179@users.noreply.github.com>
2022-02-01 17:45:09 +0300

    Task #5

--- 05-koajs/01-chat-app/app.js
+++ 05-koajs/01-chat-app/app.js
@@ -7,11 +7,22 @@ app.use(require('koa-bodyparser')());
 
 const Router = require('koa-router');
 const router = new Router();
+let clients = [];
 
 router.get('/subscribe', async (ctx, next) => {
+    ctx.body = await new Promise(resolve => {
+        clients.push(message => resolve(message))
+    })
 });
 
 router.post('/publish', async (ctx, next) => {
+    if (ctx.request.body.message) {
+        clients.forEach(resolve => {
+            resolve(ctx.request.body.message);
+        })
+        ctx.status = 200;
+        clients = [];
+    }
 });
 
 app.use(router.routes());
