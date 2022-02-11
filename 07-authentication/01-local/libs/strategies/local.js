42fed897dd00c1654c3d6eff89539db5b9c48e0f
mat179 <mat179@users.noreply.github.com>
2022-02-11 17:07:55 +0300

    Tasks #7-1 & #7-2 Local auth strategy refactor OAuth authenticate function

--- 07-authentication/01-local/libs/strategies/local.js
+++ 07-authentication/01-local/libs/strategies/local.js
@@ -4,17 +4,21 @@ const User = require('../../models/User');
 module.exports = new LocalStrategy(
     {usernameField: 'email', session: false},
     async function(email, password, done) {
-        const user = await User.findOne({email: email});
-        if (!user) {
-            done(null, false, 'Нет такого пользователя');
+        try {
+            const user = await User.findOne({email: email});
+            if (!user) {
+                return done(null, false, 'Нет такого пользователя');
+            }
+
+            const isPasswordOk = await user.checkPassword(password)
+
+            if (!isPasswordOk) {
+                return done(null, false, 'Неверный пароль');
+            }
+
+            return done(null, user);
+        } catch (err) {
+            done(err);
         }
-
-        const isPasswordOk = await user.checkPassword(password)
-
-       if (!isPasswordOk){
-           done(null, false, 'Неверный пароль');
-       }
-
-        done(null, user);
     },
 );
